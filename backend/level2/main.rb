2d2493b50ca494f4044bd5232f090e85f9e78da3
rafael kasinski <rakasin@gmail.com>
2023-03-12 17:35:13 -0300

    Did pseudocode for level 2

---
+++ backend/level2/main.rb
@@ -0,0 +1,18 @@
+# get the list of rentals
+# iterate through that list
+
+# calculate rental days
+
+# look up price per day for car
+# conditionals:
+# 1: if rental_days <= 1, get price_per_day
+# 2: if rental_days > 1 && < 4, get price_per_day with 10% discount
+# 3: if rental_days > 4 && < 10, get price_per_day with 30% discount
+# 4: if rental_days > 10, get price_per_day with 50% discount
+# multiply by price per day
+
+# look up price per km
+# multiply price per km by distance
+# get total price by adding time and distance components
+# return a hash with a key "rentals" which is an array of hashes with id and total price
+# print this out in output-backend.json file
