574b880696f18dfa720188542170b6e1a71c4680
rafael kasinski <rakasin@gmail.com>
2023-03-12 21:30:19 -0300

    Fixed level 5 pseudocode

--- backend/level5/main.rb
+++ backend/level5/main.rb
@@ -14,21 +14,27 @@
 # look up price_per_km
 # multiply price_per_km by distance
 
-# calculate gps cost to driver with rental_days * 5€/day
-# calculate baby_seat cost to driver with rental_days * 2€/day
-# calculate additional_insurance cost to driver with rental_days * 10€/day
-# if these values are too low, multiply by 100
+# look up options
+# confirm type used: gps, baby_seat, and additional_insurrance
+# conditionals:
+# if gps > 0, calculate gps cost to driver with rental_days * 5€/day
+# if baby_seat > 0, calculate baby_seat cost to driver with rental_days * 2€/day
+# if additional_insurrance > 0, calculate additional_insurance cost to driver with rental_days * 10€/day
+# obs.: if these values are too low, multiply each by 100
 
 # get total_price by adding total_time, total_distance, gps, baby_seat, and additional_insurance
 
 # look up total_price
 # driver gets total_price and is debited
+
+# look up options and type: 'gps' and 'baby_seat'
 # owner gets credited with (total_price * 0.7) + (gps * €5/day) + (baby_seat * €2/day)
 
 # commission is total_price * 0.3
 # split into three parts: insurance, assistance, and drivy
 # insurance gets credited with comission * 0.5
 # assistance gets credited with rental_days multiplied by €1/day (if result is low, multiply by €100/day)
+# look up options and type: 'additional_insurrance'
 # drivy gets credited with insurance - assistance + additional_insurance
 
 # return a hash with a key "rentals" which is an array of hashes with id, options, and actions
